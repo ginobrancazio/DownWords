cd7df8b7aed8a39faed3aca5018765608d0e0a52
ginobrancazio <gino@cartesianclassroom.com>
2025-05-19 23:30:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -705,7 +705,7 @@ function handleGameCompletion(words) {
   }
 
   const playerTimeInSeconds = timeLeft; 
-  const averageTimeInSeconds = 508;     
+  const averageTimeInSeconds = 231;     
   const blocklength = averageTimeInSeconds/8;
   
   // Get the selected date
