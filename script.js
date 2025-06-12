f2b2710810ae25efff8d5e6c5152f382a9c5d566
ginobrancazio <ginobrancazio@hotmail.com>
2025-06-12 09:21:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -705,7 +705,7 @@ function handleGameCompletion(words) {
   }
 
   const playerTimeInSeconds = timeLeft; 
-  const averageTimeInSeconds = 201;     
+  const averageTimeInSeconds = 89;     
   const blocklength = averageTimeInSeconds/8;
   
   // Get the selected date
