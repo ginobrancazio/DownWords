d72f76ec3c09dc117146e17e98455e51942050ba
ginobrancazio <ginobrancazio@hotmail.com>
2025-10-31 08:21:47 +0000

    Update average time in seconds from 176 to 401

--- script.js
+++ script.js
@@ -705,7 +705,7 @@ function handleGameCompletion(words) {
   }
 
   const playerTimeInSeconds = timeLeft; 
-  const averageTimeInSeconds = 176;     
+  const averageTimeInSeconds = 401;     
   const blocklength = averageTimeInSeconds/8;
   
   // Get the selected date
